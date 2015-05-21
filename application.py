c95da014bd6eb34acacc8ebd4348e8639f6d7920
Tadeusz Magura-Witkowski <teeed@na1noc.pl>
2015-05-21 23:00:56 +0200

    Mikrotik version

--- application.py
+++ application.py
@@ -20,9 +20,11 @@
 
 from Crypto.Cipher import AES
 from Crypto import Random
+from routeros import *
 import web
 import json
 import datetime
+import socket
 import time
 import ConfigParser
 import base64
@@ -40,41 +42,29 @@ urls = (
 app = web.application(urls, globals())
 
 def get_current_dhcp_leases():
-	# final version - parses /var/lib/dhcp/dhcp.leases
+	# mikrotik's version
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((config.get('routeros', 'host'), config.getint('routeros', 'port')))
+		apiros = ApiRos(s)
+		apiros.login(config.get('routeros', 'login'), config.get('routeros', 'password'))
 
-	data = ''
-	with file(config.get('database', 'leases_file'), 'r') as f:
-		data = f.read()
+		dhcp_leases = apiros.talk(['/ip/dhcp-server/lease/print'])
 
-	# matches = re.findall(r'lease ([^\s]*) {\n  starts \d \d\d\d\d/\d\d/\d\d \d\d:\d\d:\d\d;\n  ends \d \d\d\d\d/\d\d/\d\d \d\d:\d\d:\d\d;\n  cltt \d \d\d\d\d/\d\d/\d\d \d\d:\d\d:\d\d;\n  binding state active;[\s\S]*?hardware ethernet (.{17});', data)
-	# return [(ip, mac_to_binary(mac)) for ip, mac in matches]
-
-	# fuck regexp (which actually works well), lets try to parse it manually (in really ugly way!)
-	ipnow = ''
-	macaddr = ''
-	inblock = False
+		s.close()
+	except:
+		dhcp_leases = []
 
 	matches = set([])
 
-	utcnow = datetime.datetime.utcnow()
-
-	for line in data.split('\n'):
-		line = line.strip()
-
-		if not len(line) or line[0] == '#':
+	for lease in dhcp_leases:
+		if lease[0] != '!re':
 			continue
 
-		if line.startswith('lease '):
-			ipnow = line[6:-2]
-			inblock = True
-		elif inblock:
-			if line[0] == '}':
-				matches.add( (ipnow, macaddr.lower()) )
-				inblock = False
-			elif line.startswith('binding state free') or (line.startswith('ends') and datetime.datetime.strptime(line[7:-1], "%Y/%m/%d %H:%M:%S") < utcnow): # skip entry
-				inblock = False
-			elif line.startswith('hardware ethernet'):
-				macaddr = line[18:-1]
+		lease_data = lease[1]
+
+		# print lease_data['=mac-address'], lease_data['=active-address']
+		matches.add( (lease_data['=mac-address'], lease_data['=active-address'].lower()) )
 
 	return list(matches)
 
